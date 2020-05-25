f8df9f40f11a1de42448dd48ba3e5e3e2a7d4671
Asia Gajzler <48221938+sunrisemystery@users.noreply.github.com>
2020-05-25 09:33:32 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Initializing all modules."""
-
-__all__ = ["adminTransactionsGUI", "cars", "cars_db", "carsDisplayer", "customerEditAccount",
-           "customerLogin", "customers_db", "customersRegister", "customerTransactionGUI",
-           "shared", "transactions_db"]
